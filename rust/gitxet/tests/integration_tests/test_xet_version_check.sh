c5149bc12ee11ba1dc6d287ffb0f0a0145f1c171
Hoyt Koepke <hoytak@xethub.com>
2023-10-06 08:57:16 -0700

    Fixed failing unit tests.

--- rust/gitxet/tests/integration_tests/test_xet_version_check.sh
+++ rust/gitxet/tests/integration_tests/test_xet_version_check.sh
@@ -10,6 +10,6 @@ export _XET_VERSION_OVERRIDE="v0.0.0"
 
 remote=$(create_bare_repo)
 
-out="$(git xet clone $remote repo_1 2>&1)"
+out="$(git xet clone $remote repo_1 2>&1 > /dev/null)"  # Capture just stderr
 
-[[ $out == *"version available"* ]] || die "Version message not in $out"
+[[ $out == *"new version"* ]] || die "Version message not in $out"
