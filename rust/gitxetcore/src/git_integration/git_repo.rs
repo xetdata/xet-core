5e2fc1bb370650b8e4db9f91aa2c279df4545ec8
Hoyt Koepke <hoytak@xethub.com>
2023-10-13 09:54:34 -0700

    Added allowance for unknown lints.

--- rust/gitxetcore/src/git_integration/git_repo.rs
+++ rust/gitxetcore/src/git_integration/git_repo.rs
@@ -132,6 +132,8 @@ pub fn open_libgit2_repo(
         Some(path) => Repository::discover(path)?,
         None => Repository::open_from_env()?,
     };
+
+    #[allow(unknown_lints)]
     #[allow(clippy::arc_with_non_send_sync)]
     Ok(Arc::new(repo))
 }
