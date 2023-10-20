74856892f6a1d386edea189b4fced3a509a64b4c
Hoyt Koepke <hoytak@xethub.com>
2023-10-20 10:21:36 -0700

    Addressed PR feedback.

--- rust/gitxetcore/src/config/xet.rs
+++ rust/gitxetcore/src/config/xet.rs
@@ -410,7 +410,6 @@ impl XetConfig {
         mut self,
         smudge_query_policy: Option<SmudgeQueryPolicy>,
     ) -> Result<Self, ConfigError> {
-        // First, ensure that the cas is not local; if it is, we're working completely local
         self.smudge_query_policy = smudge_query_policy.unwrap_or_default();
         Ok(self)
     }
