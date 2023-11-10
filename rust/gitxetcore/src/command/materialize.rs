3b490b8adf2d5b58357b1f898b728ea6031e0ee0
Di <di@xetdata.com>
2023-11-10 11:01:15 -0800

    run (de)materialize anywhere and anyhow

--- rust/gitxetcore/src/command/materialize.rs
+++ rust/gitxetcore/src/command/materialize.rs
@@ -1,11 +1,16 @@
 use clap::Args;
 use lazy::lazy_pathlist_config::{check_or_create_lazy_config, LazyPathListConfigFile};
-use std::path::Path;
-use std::time::{SystemTime, UNIX_EPOCH};
-use tracing::error;
+use parutils::tokio_par_for_each;
+use pointer_file::PointerFile;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
-use crate::errors::{GitXetRepoError, Result};
-use crate::git_integration::{list_files_from_repo, run_git_captured, GitXetRepo};
+use crate::constants::MAX_CONCURRENT_DOWNLOADS;
+use crate::data_processing::PointerFileTranslator;
+use crate::errors::Result;
+use crate::git_integration::{filter_files_from_index, walk_working_dir, GitXetRepo};
 use crate::{config::XetConfig, constants::GIT_LAZY_CHECKOUT_CONFIG};
 
 #[derive(Args, Debug)]
@@ -13,7 +18,7 @@ pub struct MaterializeArgs {
     #[clap(short, long)]
     recursive: bool,
 
-    path: String,
+    path: PathBuf,
 }
 
 pub async fn materialize_command(cfg: XetConfig, args: &MaterializeArgs) -> Result<()> {
@@ -31,77 +36,82 @@ pub async fn materialize_command(cfg: XetConfig, args: &MaterializeArgs) -> Resu
     let mut lazyconfig =
         LazyPathListConfigFile::load_smudge_list_from_file(&lazy_config_path, false).await?;
 
-    // now find where we are in the working dir.
-    let pwd = std::env::current_dir()?;
-    let workdir_root = repo.repo_dir;
-    let relative_to_workroot = pwd.strip_prefix(&workdir_root).map_err(|e| {
-        error!("Failed to get relative path to the working directory root.");
-        GitXetRepoError::Other(e.to_string())
-    })?;
+    let workdir_root = repo.repo_dir.clone();
 
-    // if now in a subdir and user typed ".", the root_path should not include the "."
-    let root_path = if relative_to_workroot != Path::new("") && args.path == "." {
-        relative_to_workroot.to_owned()
-    } else {
-        relative_to_workroot.join(&args.path)
-    };
+    // now they are relative path to the working directory root
+    let path_list = walk_working_dir(&workdir_root, &args.path, args.recursive)?;
 
-    let path_list = list_files_from_repo(
-        &repo.repo,
-        root_path.to_str().unwrap_or_default(),
-        None,
-        args.recursive,
-    )?;
+    let path_list = filter_files_from_index(&path_list, repo.repo.clone())?;
 
     if path_list.is_empty() {
         eprintln!(
-            "Didn't find any files under {}, skip materializing.",
+            "Didn't find any files under {:?}, skip materializing.",
             args.path
         );
         return Ok(());
     }
 
-    eprintln!("Materializing {} files...", path_list.len());
+    eprintln!("Materializing {} file(s)...", path_list.len());
 
-    let path_list_ref: Vec<_> = path_list.iter().map(|s| s.as_str()).collect();
+    let path_list_ref: Vec<_> = path_list
+        .iter()
+        .map(|s| s.to_str().unwrap_or_default())
+        .collect();
 
     lazyconfig.add_rules(&path_list_ref);
 
     // saves changes to the file
     drop(lazyconfig);
 
-    // rerun smudge filter
+    // smudge all files
+    let absolute_path_list: Vec<_> = path_list.iter().map(|p| workdir_root.join(p)).collect();
 
-    // touch all files we want to materialize
-    for p in path_list_ref.iter() {
-        let relative_path_from_curdir =
-            Path::new(p)
-                .strip_prefix(relative_to_workroot)
-                .map_err(|e| {
-                    error!(
-                        "Failed to get relative path for {p:?} to the current working directory."
-                    );
-                    GitXetRepoError::Other(e.to_string())
-                })?;
-        touch_file(relative_path_from_curdir)?;
-    }
+    let translator = Arc::new(PointerFileTranslator::from_config(&cfg).await?);
+    let translator_ref = &translator;
 
-    // files not touched under args.path will not rerun through the filter
-    let git_args = ["--", &args.path];
+    tokio_par_for_each(
+        absolute_path_list,
+        MAX_CONCURRENT_DOWNLOADS,
+        |path, _| async move {
+            let translator = translator_ref.clone();
+            smudge_file_to_itself(&translator, &path).await
+        },
+    )
+    .await
+    .map_err(|e| match e {
+        parutils::ParallelError::JoinError => {
+            anyhow::anyhow!("Join error on smudge file")
+        }
+        parutils::ParallelError::TaskError(e) => e,
+    })?;
 
-    run_git_captured(None, "checkout", &git_args, true, None)?;
+    // update index so materialized files don't show as "Changes not staged for commit"
+    repo.run_git_checked_in_repo("add", &["-A"])?;
 
     eprintln!("Done");
 
     Ok(())
 }
 
-fn touch_file(file_path: &Path) -> anyhow::Result<()> {
-    use utime::*;
+/// Smudge a pointer file and overwrite itself
+async fn smudge_file_to_itself(
+    translator: &PointerFileTranslator,
+    path: &Path,
+) -> anyhow::Result<()> {
+    let pointer_file = PointerFile::init_from_path(path.to_str().unwrap_or_default());
 
-    let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as i64;
+    // not a pointer file, leave it as it is.
+    if !pointer_file.is_valid() {
+        return Ok(());
+    }
 
-    set_file_times(file_path, now, now)?;
+    let file_hash = pointer_file.hash()?;
+
+    let mut writer = Box::new(BufWriter::new(File::create(path)?));
+
+    translator
+        .smudge_file_from_hash(Some(path.to_owned()), &file_hash, &mut writer, None)
+        .await?;
 
     Ok(())
 }
