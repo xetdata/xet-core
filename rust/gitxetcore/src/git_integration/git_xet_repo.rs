c56ffd709c134291d6276cea47d447e75f3bb55b
Hoyt Koepke <hoytak@xetdata.com>
2023-11-10 13:51:12 -0800

    Disable recursive reference transaction hooks (#194)
    This PR disables recursive reference transaction hooks in the MerkleDB
    v2 case. It is unclear why they are needed in the MDB v1 case; this
    warrants further investigation.
    
    On the pre-push hook, this seems to save roughly 1.5 - 2 seconds of time
    depending on the scenario.
    
    ---------
    
    Co-authored-by: Hoyt Koepke <hoytak@xethub.com>

--- rust/gitxetcore/src/git_integration/git_xet_repo.rs
+++ rust/gitxetcore/src/git_integration/git_xet_repo.rs
@@ -2,10 +2,10 @@ use is_executable::IsExecutable;
 use mdb_shard::error::MDBShardError;
 use mdb_shard::shard_version::ShardVersion;
 use std::collections::{HashMap, HashSet};
-use std::fs::create_dir_all;
 #[cfg(unix)]
 use std::fs::Permissions;
-use std::fs::{self, File, OpenOptions};
+use std::fs::{self, File};
+use std::fs::{create_dir_all, OpenOptions};
 #[cfg(unix)]
 use std::os::unix::prelude::PermissionsExt;
 
@@ -71,8 +71,11 @@ lazy_static! {
 
 const PREPUSH_HOOK_CONTENT: &str =
     "git-xet hooks pre-push-hook --remote \"$1\" --remote-loc \"$2\"\n";
-const REFERENCE_TRANSACTION_HOOK_CONTENT: &str =
+
+const REFERENCE_TRANSACTION_HOOK_CONTENT_MDB_V1: &str =
     "git-xet hooks reference-transaction-hook --action \"$1\"\n";
+const REFERENCE_TRANSACTION_HOOK_CONTENT_MDB_V2: &str =
+    "[[ ! -z \"$XET_DISABLE_HOOKS\" ]] || git-xet hooks reference-transaction-hook --action \"$1\"\n";
 
 // Provides a mechanism to lock files that can often be modifiied, such as hooks,
 // .gitattributes, etc.  Normally our mechanisms should handle all these files
@@ -896,10 +899,20 @@ impl GitXetRepo {
             }
 
             if !content.contains(script) {
-                let mut file = OpenOptions::new().append(true).open(&path)?;
+                let mut file = OpenOptions::new().write(true).open(&path)?;
+
+                for line in content.lines() {
+                    if !line.contains("git-xet hooks") {
+                        writeln!(file, "{line}")?;
+                    }
+                }
                 writeln!(file, "{script}")?;
+
                 changed = true;
-                info!("Adding hooks to file {:?}, appending to end.", subpath);
+                info!(
+                    "Adding hooks to file {:?}, filtering and appending to end.",
+                    subpath
+                );
             }
         }
 
@@ -938,7 +951,12 @@ impl GitXetRepo {
     pub fn write_reference_transaction_hook(&self) -> Result<bool> {
         self.write_hook(
             "hooks/reference-transaction",
-            REFERENCE_TRANSACTION_HOOK_CONTENT,
+            match self.mdb_version {
+                ShardVersion::V1 => REFERENCE_TRANSACTION_HOOK_CONTENT_MDB_V1,
+                ShardVersion::V2 | ShardVersion::Uninitialized => {
+                    REFERENCE_TRANSACTION_HOOK_CONTENT_MDB_V2
+                }
+            },
         )
     }
 
@@ -949,7 +967,11 @@ impl GitXetRepo {
 
         let mut hook_erase_content: HashSet<&str> = HashSet::new();
 
-        for hook_line in &[PREPUSH_HOOK_CONTENT, REFERENCE_TRANSACTION_HOOK_CONTENT] {
+        for hook_line in &[
+            PREPUSH_HOOK_CONTENT,
+            REFERENCE_TRANSACTION_HOOK_CONTENT_MDB_V1,
+            REFERENCE_TRANSACTION_HOOK_CONTENT_MDB_V2,
+        ] {
             for s in hook_line
                 .lines()
                 .map(|s| s.trim())
