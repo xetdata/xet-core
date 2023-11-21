c3dba478e9cec88e76186077a22c741cc0d13f1b
Hoyt Koepke <hoytak@xetdata.com>
2023-11-21 15:03:35 -0800

    Updated test_xet_version_check.sh to be robust to API rate limits. (#206)
    test_xet_version_check pings the github server a lot, resulting in
    denials due to API rate limits. This new version uses a local file cache
    to reduce this query to once ever 24 hours or so, and then passes still
    if the query hits such API rate limits.
    
    ---------
    
    Co-authored-by: Hoyt Koepke <hoytak@xethub.com>

--- rust/gitxet/tests/integration_tests/test_xet_version_check.sh
+++ rust/gitxet/tests/integration_tests/test_xet_version_check.sh
@@ -2,19 +2,72 @@
 set -e
 set -x
 
+global_tmp_dir=/tmp
+
+if [[ ! -z ${TEMP} ]] ; then
+  global_tmp_dir="$TEMP"
+elif [[ ! -z ${TEMP_DIR} ]] ; then 
+  global_tmp_dir="$TEMP_DIR"
+elif [[ ! -z ${TMPDIR} ]] ; then
+  global_tmp_dir="$TMPDIR"
+fi
+
+# Set this here, before HOME gets reset to isolate git.
+xet_version_info_file_full="${global_tmp_dir}/git_xet_version_check_api_cache_full"
+xet_version_info_file_latest="${global_tmp_dir}/git_xet_version_check_api_cache_latest"
+
+github_api_url_full="https://api.github.com/repos/xetdata/xet-tools/releases"
+github_api_url_latest="https://api.github.com/repos/xetdata/xet-tools/releases/latest"
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 . "$SCRIPT_DIR/initialize.sh"
 setup_basic_run_environment
 
-if [[ ! -z $(ping -c 1 github.com 2>&1 | grep -i "unknown host") ]] ; then 
-  echo "github.com unreachable, skipping test."
-  exit 0
+download_new_version_file () { 
+  local version_url="$1"
+  local version_file="$2"
+  curl -L "$version_url" > "$version_file.temp"
+
+  if [[ ! -z $(grep "API rate limit exceeded" "$version_file.temp") ]] ; then
+    >&2 echo "Skipping update of cache of local information; API rate limit exceded."
+    rm -f "$version_file.temp"
+  elif [[ -z $(grep -l "github.com/xetdata/xet-tools/releases" "$version_file.temp") ]] ; then 
+    >&2 echo "Skipping update of cache of local information; downloaded file does not contain the correct info." 
+    rm -f "$version_file.temp"
+  else 
+    mv "$version_file.temp" "$version_file" 
+  fi
+}
+
+refresh_version_file() {
+  local version_url="$1"
+  local version_file="$2"
+
+# To avoid unneeded pings of the github API, without causing tests to fail, attempt 
+if [[ -e "$file" ]] ; then 
+  # If it has been written in the last 24 hours, just use that.  Otherwise, download a new version.
+  if [[ ! -z $(find "$file" -mtime +1d -print) ]] ; then
+    download_new_version_file "$version_url" "$version_file"
+  fi
+else 
+    download_new_version_file "$version_url" "$version_file"
 fi
 
+}
+
+refresh_version_file $github_api_url_full $xet_version_info_file_full
+refresh_version_file $github_api_url_latest $xet_version_info_file_latest
+
+if [[ ! -e "$xet_version_info_file_full" ]] ||  [[ ! -e "$xet_version_info_file_latest" ]]; then
+  2<&1 echo "Error downloading version info information; skipping test."
+  exit 0
+fi
 
 unset XET_DISABLE_VERSION_CHECK
 export _XET_TEST_VERSION_OVERRIDE="v0.0.0"
 export XET_UPGRADE_CHECK_FILENAME=`pwd`/.xet/xet_version_info
+export _XET_VERSION_QUERY_URL_OVERRIDE_FULL="file://$xet_version_info_file_full"
+export _XET_VERSION_QUERY_URL_OVERRIDE_LATEST="file://$xet_version_info_file_latest"
 
 env
 
