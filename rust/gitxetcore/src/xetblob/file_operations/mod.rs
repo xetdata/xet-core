8cc1558bcc53e1569f157ac2ea7e88832a481f7f
Hoyt Koepke <hoytak@xethub.com>
2024-02-13 14:47:14 -0800

    Rename write_file_handle.

--- rust/gitxetcore/src/xetblob/file_operations/mod.rs
+++ rust/gitxetcore/src/xetblob/file_operations/mod.rs
@@ -1,9 +1,9 @@
 mod batch_operations;
+mod batched_wfile_handle;
 mod cp_operations;
 mod fs_interface;
-mod write_file_handle;
 mod write_transaction_wrapper;
 
 pub use batch_operations::XetRepoOperationBatch;
+pub use batched_wfile_handle::BatchedWriteFileHandle;
 pub use cp_operations::perform_copy;
-pub use write_file_handle::BatchedWriteFileHandle;
