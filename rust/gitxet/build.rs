de836e712d450937d50df58efca0b5e617c8337b
Hoyt Koepke <hoytak@xethub.com>
2024-02-16 11:28:57 -0800

    Added build.rs that standardizes stack size to 8MB.

---
+++ rust/gitxet/build.rs
@@ -0,0 +1,15 @@
+// build.rs to set up environment variables for running all this.
+use std::env;
+
+fn main() {
+    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or("".to_owned());
+
+    if target_env == "msvc" {
+        println!("cargo:rustc-link-arg=/stack:{}", 8 * 1024 * 1024);
+    } else if target_env == "" || target_env == "gnu" {
+        println!(
+            "cargo:rustc-link-arg=-Wl,-stack_size,{:#X}",
+            8 * 1024 * 1024
+        );
+    }
+}
