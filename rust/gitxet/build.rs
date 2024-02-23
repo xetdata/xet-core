63b03a2209554e73b67979ce61dfd3cb49ab322a
Hoyt Koepke <hoytak@xethub.com>
2024-02-23 15:52:12 -0800

    Changed build.rs script so that it reruns well.

--- rust/gitxet/build.rs
+++ rust/gitxet/build.rs
@@ -2,9 +2,12 @@
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs")
+    
     let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or("".to_owned());
 
     if target_env == "msvc" {
         println!("cargo:rustc-link-arg=/stack:{}", 8 * 1024 * 1024);
     }
+
 }
