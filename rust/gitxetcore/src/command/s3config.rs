8826092f1736fac05609fa1d0f9b048d77e388d1
seanses <di@xetdata.com>
2024-04-18 17:28:28 -0700

    terminal emulators on Windows are annoying

--- rust/gitxetcore/src/command/s3config.rs
+++ rust/gitxetcore/src/command/s3config.rs
@@ -78,12 +78,11 @@ source "{}"                       # For fish
     eprintln!(
         r#"1. Profile for the XetHub S3 Service is configured now. Great!
 
-To get started source the corresponding env file. This is usually done by
-running one of the following (note the leading DOT):
-. "{}"                            # For git-bash/Cygwin/PowerShell
+To get started source the corresponding env file. This can be done by running
+the following:
 "{}"                              # For cmd
         "#,
-        path_str, path_str,
+        path_str,
     );
     eprintln!(
         r#"2. To see the repos you have access to via XetHub S3, run: 
