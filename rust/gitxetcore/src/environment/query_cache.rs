dcbf1fb04c0d7e19442e201307d072902042379b
Hoyt Koepke <hoytak@xethub.com>
2024-05-28 23:40:39 -0700

    Cache CAS endpoint query.

---
+++ rust/gitxetcore/src/environment/query_cache.rs
@@ -0,0 +1,208 @@
+use crate::config::permission::Permission;
+use crate::errors::Result;
+use chrono::{DateTime, Utc};
+use error_printer::ErrorPrinter;
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
+use std::path::{Path, PathBuf};
+use tracing::debug;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct QueryValuesImpl {
+    query_time: DateTime<Utc>,
+    value: String,
+}
+
+impl QueryValuesImpl {
+    fn query_age_in_seconds(&self) -> u64 {
+        Utc::now()
+            .signed_duration_since(self.query_time)
+            .num_seconds()
+            .max(0) as u64
+    }
+}
+
+pub struct CachedQueryWrapper {
+    file_name: PathBuf,
+    query_value: Option<QueryValuesImpl>,
+    max_valid_seconds: u64,
+}
+
+impl CachedQueryWrapper {
+    /// Cache a value with a timeout for persistance between runs of the program.
+    ///
+    /// cache_dir: the directory where the cache should live (config.xet_home is good).
+    /// key_name: the name for this query.
+    /// max_valid_seconds: The maximum number of seconds for which to load and return an existing value.
+    ///
+    /// Usage:
+    ///
+    ///   let mut query_cache = CachedQueryWrapper::new(...);
+    ///
+    ///   let value = {
+    ///      if let Some(value) = query_cache.has_valid_value() {
+    ///         value
+    ///      } else {
+    ///         let value = query_for_value(...); // get the value using an expensive operation
+    ///         query_cache.set_value(value.clone());
+    ///         value
+    ///      }
+    ///   };
+    ///
+    ///
+    pub fn new(
+        cache_dir: impl AsRef<Path>,
+        key_name: &str,
+        max_valid_seconds: u64,
+    ) -> Result<Self> {
+        let file_name = cache_dir.as_ref().join("query_cache").join(key_name);
+        Permission::current().create_dir_all(file_name.parent().unwrap())?;
+
+        Ok(Self {
+            query_value: None,
+            file_name,
+            max_valid_seconds,
+        })
+    }
+
+    /// Returns the value if it has been set or is cached and still valid.
+    pub fn get(&mut self) -> Option<String> {
+        if let Some(qv) = self.query_value.as_ref() {
+            return Some(qv.value.clone());
+        }
+
+        let Some(qv) = self.attempt_load_from_file() else {
+            return None;
+        };
+
+        if qv.query_age_in_seconds() < self.max_valid_seconds {
+            let ret = Some(qv.value.clone());
+            self.query_value = Some(qv);
+            ret
+        } else {
+            None
+        }
+    }
+
+    fn attempt_load_from_file(&self) -> Option<QueryValuesImpl> {
+        // if file doesn't exist return None
+
+        if !self.file_name.exists() {
+            debug!("{:?} does not exist; skipping load.", &self.file_name);
+            return None;
+        }
+
+        let Ok(file_contents) = std::fs::read_to_string(&self.file_name)
+            .debug_error(format!("Error reading version file {:?}", &self.file_name))
+        else {
+            return None;
+        };
+
+        let Ok(qv) = serde_json::from_str::<QueryValuesImpl>(&file_contents).info_error(format!(
+            "Error decoding version file contents from {:?}",
+            self.file_name
+        )) else {
+            debug!(
+                "Failed to parse query check information from {:?}.",
+                self.file_name
+            );
+            return None;
+        };
+
+        debug!("Loaded cached query information {qv:?}.");
+
+        Some(qv)
+    }
+
+    /// Verifies that the
+    pub fn set(&mut self, value: String) -> Result<()> {
+        let qv = QueryValuesImpl {
+            value,
+            query_time: Utc::now(),
+        };
+
+        // Now, save out the information here.
+        if let Ok(json_string) = serde_json::to_string_pretty(&qv).map_err(|e| {
+            debug!("Error serializing query values to JSON: {:?}", e);
+            e
+        }) {
+            debug!(
+                "CachedQueryInfo:save: Serializing cached query struct {qv:?} to {:?}",
+                &self.file_name
+            );
+
+            let _ = std::fs::write(&self.file_name, json_string).map_err(|e| {
+                debug!(
+                    "QueryCacheError:save: Error writing current version info to file to {:?}: {e:?}",
+                    &self.file_name
+                );
+                e
+            });
+        }
+
+        self.query_value = Some(qv);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_set_and_get_value() -> Result<()> {
+        let temp_dir = tempdir()?;
+        let cache_dir = temp_dir.path().join("cache");
+        let key_name = "test_key";
+        let max_valid_seconds = 60;
+
+        let mut query_cache = CachedQueryWrapper::new(&cache_dir, key_name, max_valid_seconds)?;
+        let test_value = "test_value".to_string();
+
+        query_cache.set(test_value.clone())?;
+
+        // Create a new instance to force load from disk
+        let mut query_cache = CachedQueryWrapper::new(&cache_dir, key_name, max_valid_seconds)?;
+        let cached_value = query_cache.get();
+
+        assert_eq!(cached_value, Some(test_value));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_force_load_from_cache() -> Result<()> {
+        let temp_dir = tempdir()?;
+        let cache_dir = temp_dir.path().join("cache");
+        let key_name = "test_key";
+        let max_valid_seconds = 0; // Force load from cache, but expired
+
+        let mut query_cache = CachedQueryWrapper::new(&cache_dir, key_name, max_valid_seconds)?;
+        let test_value = "test_value".to_string();
+
+        query_cache.set(test_value.clone())?;
+
+        // Create a new instance to force load from disk
+        let mut query_cache = CachedQueryWrapper::new(&cache_dir, key_name, max_valid_seconds)?;
+        let cached_value = query_cache.get();
+        assert_eq!(cached_value, None);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_value_from_empty_cache() -> Result<()> {
+        let temp_dir = tempdir()?;
+        let cache_dir = temp_dir.path().join("cache");
+        let key_name = "test_key";
+        let max_valid_seconds = 60;
+
+        let mut query_cache = CachedQueryWrapper::new(&cache_dir, key_name, max_valid_seconds)?;
+        let cached_value = query_cache.get();
+        assert_eq!(cached_value, None);
+
+        Ok(())
+    }
+}
--- rust/gitxetcore/src/xetblob/remote_repo_info.rs
+++ rust/gitxetcore/src/xetblob/remote_repo_info.rs
@@ -1,9 +1,7 @@
-use std::hash::Hash;
-
 use super::bbq_queries::git_remote_to_base_url;
 use super::BbqClient;
 use crate::config::XetConfig;
-use crate::environment::query_cache::{cachable_query_value, CachedQueryWrapper};
+use crate::environment::query_cache::CachedQueryWrapper;
 use anyhow::anyhow;
 use serde::{Deserialize, Serialize};
 use tracing::debug;
@@ -66,18 +64,23 @@ pub async fn get_cas_endpoint_from_git_remote(
         &blake3::hash(format!("{url:?}").as_bytes()).to_hex()[..16]
     );
 
-    // Now attempt to load it from cas
-    cachable_query_value(
+    let mut query_cache = CachedQueryWrapper::new(
         &config.xet_home,
         &key,
         REMOTE_CAS_ENDPOINT_QUERY_VALID_SECONDS,
-        Box::new(|| async move {
-            let bbq_client =
-                BbqClient::new().map_err(|_| anyhow!("Unable to create network client."))?;
-            get_repo_info(&url, &bbq_client)
-                .await
-                .map(|(info, _)| info.xet.cas)
-        }),
-    )
-    .await
+    )?;
+
+    if let Some(endpoint) = query_cache.get() {
+        Ok(endpoint)
+    } else {
+        let bbq_client =
+            BbqClient::new().map_err(|_| anyhow!("Unable to create network client."))?;
+
+        let endpoint = get_repo_info(&url, &bbq_client)
+            .await
+            .map(|(info, _)| info.xet.cas)?;
+
+        query_cache.set(endpoint.clone())?;
+        Ok(endpoint)
+    }
 }
