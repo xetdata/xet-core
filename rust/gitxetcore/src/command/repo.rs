fabc37de2c003a131906747e171e2b73e5920e54
Hoyt Koepke <hoytak@xethub.com>
2024-06-18 08:10:01 -1000

    Update the help text on git xet repo migrate.

--- rust/gitxetcore/src/command/repo.rs
+++ rust/gitxetcore/src/command/repo.rs
@@ -246,20 +246,6 @@ async fn migrate_command(config: XetConfig, args: &MigrateArgs) -> Result<()> {
         );
     }
 
-    eprintln!("Syncing tags.");
-    run_git_passthrough(
-        Some(&dest_dir),
-        "push",
-        &["origin", "--force", "--tags", "--follow-tags"],
-        true,
-        false,
-        Some(&[("XET_DISABLE_HOOKS", "0")]),
-    ).map_err(|e| {
-        eprintln!("Error pushing to remote.");
-        eprintln!("Please go to directory {dest_dir:?} and run `git push origin --force --tags --follow-tags --all` to push manually.");
-        e
-    })?;
-
     if !ref_list.is_empty() {
         eprintln!("Syncing remaining references.");
         let mut args = vec!["origin".to_owned()];
@@ -281,6 +267,20 @@ async fn migrate_command(config: XetConfig, args: &MigrateArgs) -> Result<()> {
         e
     })?;
     }
+
+    eprintln!("Syncing tags.");
+    run_git_passthrough(
+        Some(&dest_dir),
+        "push",
+        &["origin", "--force", "--tags", "--follow-tags"],
+        true,
+        false,
+        Some(&[("XET_DISABLE_HOOKS", "0")]),
+    ).map_err(|e| {
+        eprintln!("Error pushing to remote.");
+        eprintln!("Please go to directory {dest_dir:?} and run `git push origin --force --tags --follow-tags --all` to push manually.");
+        e
+    })?;
 
     if !args.no_cleanup {
         eprintln!("Cleaning up.");
